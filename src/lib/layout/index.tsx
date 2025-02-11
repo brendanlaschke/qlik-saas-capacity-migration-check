4a53dc32269abc0f5aed5a0bf73415545ef9011d
brendanlaschke <brendan.laschke@googlemail.com>
2025-02-11 11:06:09 +0100

    clientonly

--- src/lib/layout/index.tsx
+++ src/lib/layout/index.tsx
@@ -1,25 +1,25 @@
 'use client';
 
-import { Box } from '@chakra-ui/react';
+import { Box, ClientOnly } from '@chakra-ui/react';
 import type { ReactNode } from 'react';
 
 import { Footer } from './components/footer';
 import { Header } from './components/header';
 
 type LayoutProps = {
-  children: ReactNode;
+    children: ReactNode;
 };
 
 export const Layout = ({ children }: LayoutProps) => {
-  return (
-    <Box margin="0 auto" maxWidth={800} transition="0.5s ease-out">
-      <Box padding="8">
-        <Header />
-        <Box as="main" marginY={22}>
-          {children}
+    return (
+        <Box margin="0 auto" maxWidth={800} transition="0.5s ease-out">
+            <Box padding="8">
+                <Header />
+                <Box as="main" marginY={22}>
+                    <ClientOnly>{children}</ClientOnly>
+                </Box>
+                <Footer />
+            </Box>
         </Box>
-        <Footer />
-      </Box>
-    </Box>
-  );
+    );
 };
